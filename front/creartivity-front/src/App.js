432c0a5b0ed599529d3be6e601274fb542be0094
Zachariah Lowe <lowezake@gmail.com>
2017-11-01 15:28:45 -0600

    updated changes, beginning to

--- front/creartivity-front/src/App.js
+++ front/creartivity-front/src/App.js
@@ -5,17 +5,30 @@ import './App.css';
 import Navbar from './NavBar';
 import Jumbo from './Jumbotron';
 import Footer from './footer';
+import Canvas from './Canvas';
 import ModalSignLogin from './modal';
 
 
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {showPanel:'hidden'}
+  }
+
+  toggle = () => {
+    this.setState({
+      showPanel: !this.state.showPanel
+    });
+}
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <Jumbo />
-
+        <Canvas className="hidden"/>
         <Footer />
       </div>
     );
---
+++ front/creartivity-front/src/Canvas.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+import PropTypes from 'prop-types'
+
+const Canvas = (props) => {
+
+  return (
+<div >
+TEST
+</div>
+  );
+};
+
+
+
+export default Canvas;
