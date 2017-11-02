acddbdaa65fca579cf426e17fe8a46fc7f929f57
Nate <n8bdoesemailvia@gmail.com>
2017-11-02 14:03:00 -0600

    canvas

--- front/creartivity-front/src/Canvas.js
+++ front/creartivity-front/src/Canvas.js
@@ -1,16 +1,38 @@
 import React from 'react';
-import { Button } from 'reactstrap';
-import PropTypes from 'prop-types'
+import {Button} from 'reactstrap';
+import PropTypes from 'prop-types';
 
 const Canvas = (props) => {
 
   return (
-<div >
-TEST
-</div>
+    <div className="canvasDiv">
+
+      <canvas className="canvas" width={500} height={500}></canvas>
+
+
+        <div className="button">
+          <Button  color="primary" size="lg"><i className="fa fa-step-backward text-white"></i>  Go Back</Button>{' '}
+          <Button  color="primary"  size="lg">  Cre<span className="createorlog5">ART    </span>    <i className="fa fa-step-forward text-white"></i></Button>{' '}
+        </div>
+
+    </div>
+
   );
 };
 
+Button.propTypes = {
+  active: PropTypes.bool,
+  block: PropTypes.bool,
+  color: PropTypes.string, // default: 'secondary'
+  disabled: PropTypes.bool,
 
+  // Pass in a Component to override default button element
+  // example: react-router Link
+  // default: 'button'
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+
+  onClick: PropTypes.func,
+  size: PropTypes.string
+}
 
 export default Canvas;
